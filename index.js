07918df007d09993ea69e64f2a2ef972b389faab
Jessica <jchung722@gmail.com>
2016-11-18 14:24:15 -0800

    base requirements met ~

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+$(document).ready(function() {
+
+  var myTime = setInterval(function(){time();}, 1000);
+
+  var time = function(){
+    var rightNow = new Date();
+    $('#clock').empty();
+    $('#clock').append("<p>" + rightNow + "</p>");
+  };
+
+  // setInterval('window.location.reload()', 1000); tried and removed because it's too laggy.
+});
