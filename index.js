3ca792bb133d40d82c973aa76aef2988b2381d6d
laurenfb <laurenfriesbrink@gmail.com>
2016-11-18 14:01:07 -0800

    implemented clock

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+
+var padding = function(number) {
+  if (number.toString().length == 1) {
+    number = "0" + number;
+  }
+  return number;
+};
+
+var clock = function() {
+  var now = new Date();
+  $('#clock').html(now.getHours() + ":" + padding(now.getMinutes()) + ":" + padding(now.getSeconds()));
+};
+
+var interval = window.setInterval(clock, 500);
+
+module.exports = index;
