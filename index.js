4d9893565088c34348d0a847a1c2d287b13ba6a4
Maya Wang <salome.neptune@gmail.com>
2016-11-18 15:44:38 -0800

    completed baseline

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+$(document).ready( function() {
+
+  var clock = function() {
+    $("#clock").empty();
+
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+
+    var checkFormat = function(time) {
+       if (time < 10 ) {
+         time = "0" + time;
+       }
+       return time;
+     }
+
+    hours = checkFormat(hours);
+    minutes = checkFormat(minutes);
+    seconds = checkFormat(seconds);
+
+    $("#clock").append(hours + ":" + minutes + ":" + seconds)
+  }
+
+  clock();
+  setInterval(clock, 1000);
+
+});
