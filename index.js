b758d7c4c49199b1803ea3343d766ff8ee2c5333
Briana Eng <brianaeng@gmail.com>
2016-11-18 13:47:34 -0800

    Adding note to self

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ var getTime = function() {
   $("#clock").html(currentTime);
 };
 
+//Need to use getTime without the parentheses because getTime() returns the time, when really we want setInterval to call the function itself (aka just getTime)
 var loopTime = setInterval(getTime, 1000);
