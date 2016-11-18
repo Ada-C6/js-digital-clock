64a1632f44cbec2add42d6111562efe18232818c
Briana Eng <brianaeng@gmail.com>
2016-11-18 13:41:54 -0800

    Clock working

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code here
+var getTime = function() {
+  var getDate = new Date();
+  var currentTime = getDate.toLocaleString();
+
+  $("#clock").html(currentTime);
+};
+
+var loopTime = setInterval(getTime, 1000);
