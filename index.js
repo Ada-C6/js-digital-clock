4033f68b7f02e87aa2f8e3f772e0d07822fcfdb6
Jessica <jchung722@gmail.com>
2016-11-18 15:15:41 -0800

    second iteration of requirements missing date

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ $(document).ready(function() {
   var myTime = setInterval(function(){time();}, 1000);
 
   var time = function(){
-    var rightNow = new Date();
+    var rightNow = new Date(),
+        hour = rightNow.getHours(),
+        minute = rightNow.getMinutes(),
+        second = rightNow.getSeconds(),
+        standardHour = hour % 12 || 12,
+        standardMinute = minute < 10 ? "0" + minute : minute,
+        standardSecond = second < 10 ? "0" + second : second,
+        amPm = hour < 12 ? "AM":"PM";
     $('#clock').empty();
-    $('#clock').append("<p>" + rightNow + "</p>");
+    $('#clock').append("<p>" + standardHour + ":" + standardMinute + ":" + standardSecond + " " + amPm + "</p>");
   };
 
   // setInterval('window.location.reload()', 1000); tried and removed because it's too laggy.
