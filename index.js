7881eedb089d8f5dc1338f5ad48442d5208cd6d8
Melissa Rodriguez <olenellina@gmail.com>
2016-11-18 14:01:17 -0800

    fixing delay when user first visits page

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 $(document).ready(function()
 {
+// Ensuring that there is no delay in bringing up the clock when a user first visits the page
+// The values will be refreshed within the refreshID function every second thereafter
+  var d = new Date();
+  var month = d.getMonth()+1;
+  var day = d.getDate();
+  var year = d.getFullYear()
+  var time = d.toLocaleTimeString();
+
+  $('#clock').html(time + " " + month +  "-" + day + "-" + year);
+
     var refreshId = setInterval( function()
     {
-        var now = new Date($.now());
-        $('#clock').html(now);
+        d = new Date();
+        month = d.getMonth()+1;
+        day = d.getDate();
+        year = d.getFullYear()
+        time = d.toLocaleTimeString();
+
+        $('#clock').html(time + " " + month +  "-" + day + "-" + year);
     }, 1000);
 });
