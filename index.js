b0d67a2278d6e10b42aedf96c52785cdbe264ec5
Emily Abrash <emabrash@gmail.com>
2016-11-18 13:52:07 -0800

    Working with four timezones but with big ugly combined display

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 $(document).ready( function() {
-  getTime();
+  getTime(['America/Los_Angeles', "America/New_York", "America/Denver","Europe/London"]);
   var interval = setInterval(getTime, 1000);
 });
 
 
-var getTime = function(){
+var getTime = function(zones){
   var now = new Date();
   hours = now.getHours();
   minutes = now.getMinutes();
   seconds = now.getSeconds();
 
+  var clocks = "";
+
+  for (var i = 0; i < zones.length; i++){
+    var format = "HH:mm:ss";
+    var currentTime = moment(now, format).tz(zones[i]).format(format);
+    clocks += zones[i] + ": " + currentTime + " "
+  }
+
   // Number formatting from: http://stackoverflow.com/questions/8043026/javascript-format-number-to-have-2-digit
 
-  $('#clock').text(("0" + hours).slice(-2) + ":" + ("0" + minutes).slice(-2) + ":" + ("0" + seconds).slice(-2));
+  $('.clockface').text(clocks);
 }
 
 // $('h2').click(function (){
