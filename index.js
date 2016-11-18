fe92228ffa1413e473f8d0c640ccad8328199a40
laurenfb <laurenfriesbrink@gmail.com>
2016-11-18 14:16:25 -0800

    adde am & pm, as well as removed military hours

--- index.js
+++ index.js
@@ -7,9 +7,25 @@ var padding = function(number) {
   return number;
 };
 
+var nonMilitary = function(item) {
+  if (item > 12) {
+    item = item - 12
+  };
+  return item;
+};
+
+var amOrPm = function() {
+  var now = new Date();
+  if (now.getHours() >= 12) {
+    return "pm";
+  } else {
+    return "am";
+  }
+};
+
 var clock = function() {
   var now = new Date();
-  $('#clock').html(now.getHours() + ":" + padding(now.getMinutes()) + ":" + padding(now.getSeconds()));
+  $('#clock').html(nonMilitary(now.getHours()) + ":" + padding(now.getMinutes()) + ":" + padding(now.getSeconds()) + " " + amOrPm());
 };
 
 var interval = window.setInterval(clock, 500);
