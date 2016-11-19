f901d4f55023db7534f6bb08cce34f0b91853c2f
Melissa Rodriguez <olenellina@gmail.com>
2016-11-18 21:49:45 -0800

    refactored to clean up and reuse code

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-$(document).ready(function()
-{
-// Ensuring that there is no delay in bringing up the clock when a user first visits the page
-// The values will be refreshed within the refreshID function every second thereafter
+$(document).ready(function() {
+
+var updateClock = function() {
   var d = new Date();
   var month = d.getMonth()+1;
   var day = d.getDate();
@@ -9,15 +8,10 @@ $(document).ready(function()
   var time = d.toLocaleTimeString();
 
   $('#clock').html(time + " " + month +  "-" + day + "-" + year);
+};
 
-    var refreshId = setInterval( function()
-    {
-        d = new Date();
-        month = d.getMonth()+1;
-        day = d.getDate();
-        year = d.getFullYear()
-        time = d.toLocaleTimeString();
+updateClock();
 
-        $('#clock').html(time + " " + month +  "-" + day + "-" + year);
-    }, 1000);
+setInterval(updateClock, 1000);
+
 });
