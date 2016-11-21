9825a7747d4631b28f58d63a9dca45a64d50e3a0
Brandi Phillips <milligan.brandi@gmail.com>
2016-11-20 20:23:26 -0800

    clock hours, min and seconds are functioning

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code here
+
+
+var clock = new Date();
+var cHours = clock.getHours();
+var cMinutes = clock.getMinutes();
+var cSeconds = clock.getSeconds();
+console.log(clock);
+console.log(cHours);
+console.log(cMinutes);
+console.log(cSeconds);
+
+// var element =
+//
+// document.getElementById("clock");
+
+// $(document).ready(function() {};
