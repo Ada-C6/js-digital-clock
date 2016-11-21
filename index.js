c06f341436798508afdc275671b7554cacc5a162
Rachel <rachel.l.pavilanis@gmail.com>
2016-11-21 10:25:04 -0800

    finished clock with 4 timezones.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+// function to display the time using Date.now
+var currentTime = function() {
+
+  var currentTimeWest = new Date().toLocaleString();
+  var currentTimeAsia = new Date().toLocaleString('en-US', { timeZone: 'Asia/Jakarta' });
+  var currentTimeEast = new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
+  var currentTimeLondon = new Date().toLocaleString("en-US", {timeZone: "Europe/London"});
+
+
+  $('#west').html("Pacific Standard Time: " + currentTimeWest);
+  $('#east').html("Eastern Standard Time: " + currentTimeEast);
+  $('#london').html("London: " + currentTimeLondon);
+  $('#asia').html("Asia/Jakarta: " + currentTimeAsia);
+};
+
+// sets interval - will call currentTime once a second
+var intervalID = window.setInterval(currentTime, 1000);
