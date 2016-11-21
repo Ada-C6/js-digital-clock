919084dab7f559e96c1fbb30e1a948890fe3280c
Alyssa Hursh <akhursh@gmail.com>
2016-11-21 15:51:43 -0800

    Uhhhh oops forgot any and all commits. Clock baseline works

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+
+var nIntervId;
+
+var timeKeeper = function() {
+  var now = new Date(Date.now());
+  var target = document.getElementById('clock');
+  target.innerHTML = now.getHours().toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+now.getMinutes().toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+now.getSeconds().toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+};
+
+function updateClock() {
+  nIntervId = setInterval(timeKeeper, 1000);
+}
+
+updateClock();
