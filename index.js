11751994990d459ce0713c1e95c774211e83a38e
Brandi Phillips <milligan.brandi@gmail.com>
2016-11-21 08:00:37 -0800

    clock function is written but not showing on browser

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
+// document.getElementById("clock");
+var clock = function(){
+  var currentTime = new Date();
+  var cHours = currentTime.getHours();
+  var cMinutes = currentTime.getMinutes();
+  var cSeconds = currentTime.getSeconds();
 
+  // make the time show like a clock:
+  cMinutes = (cMinutes < 10 ? '0' : '') + cMinutes;
+  cSeconds = (cSeconds < 10 ? '0' : '') + cSeconds;
 
-var clock = new Date();
-var cHours = clock.getHours();
-var cMinutes = clock.getMinutes();
-var cSeconds = clock.getSeconds();
-console.log(clock);
-console.log(cHours);
-console.log(cMinutes);
-console.log(cSeconds);
+  var timeOfDay = (cHours < 12)? 'AM' : 'Pm';
+  cHours = ( cHours > 12)? cHours - 12 : cHours;
+  cHours = (currentHours === 0)? 12 : cHours;
+  var cTimeString = cHours + ':' + cMinutes + ':' + cSeconds + ' ' + timeOfDay;
+  $('#clock').html(cTimeString);
+};
+
+// console.log(clock);
+// console.log(cHours);
+// console.log(cMinutes);
+// console.log(cSeconds);
 
 // var element =
 //
 // document.getElementById("clock");
 
-// $(document).ready(function() {};
+$(document).ready(function()
+{
+   setInterval('updateClock()', 1000);
+});
