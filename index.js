f23a26b82048c94b5c7969ed272418bcbf189294
Jessica <jchung722@gmail.com>
2016-11-21 11:16:54 -0800

    added timezones

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 // Your code here
 $(document).ready(function() {
 
-  var myTime = setInterval(function(){time();}, 1000);
+  var seattleTime = setInterval(function(){time("Seattle", "WA", 8);}, 1000);
+  var chicagoTime = setInterval(function(){time("Chicago", "IL", 10);}, 1000);
+  var parisTime = setInterval(function(){time("Paris", "France", 17);}, 1000);
+  var seoulTime = setInterval(function(){time("Seoul", "Korea", 25);}, 1000);
 
-  var time = function(){
-    var rightNow = new Date(),
+  var time = function(city, state, offset){
+    var d = new Date();
+    var utc = d.getTime() - (d.getTimezoneOffset() * 60000);
+    var rightNow = new Date(utc + (3600000 * offset));
         day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][rightNow.getDay()],
         date = rightNow.getDate(),
         month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][rightNow.getMonth()],
@@ -16,9 +21,10 @@ $(document).ready(function() {
         standardMinute = minute < 10 ? "0" + minute : minute,
         standardSecond = second < 10 ? "0" + second : second,
         amPm = hour < 12 ? "AM":"PM";
-    $('#clock').empty();
-    $('#clock').append("<p>" + day + "<br />" + month + " " + date + ", " + year + "</p>")
-    $('#clock').append("<p>" + standardHour + ":" + standardMinute + ":" + standardSecond + " " + amPm + "</p>");
+    $('#' + city).empty();
+    $('#' + city).append("<h3>" + city + ", " + state + "</h3>");
+    $('#' + city).append("<p>" + day + "<br />" + month + " " + date + ", " + year + "</p>");
+    $('#' + city).append("<p>" + standardHour + ":" + standardMinute + ":" + standardSecond + " " + amPm + "</p>");
   };
 
   // setInterval('window.location.reload()', 1000); tried and removed because it's too laggy.
